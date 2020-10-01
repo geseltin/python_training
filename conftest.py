a48a2700f3dbefb42cfb9951b78eed63ce44802f
Alexander <GeseltinAV@gmail.com>
2020-10-01 21:01:17 +0300

    Организация модулей

---
+++ conftest.py
@@ -0,0 +1,7 @@
+import pytest
+
+@pytest.fixture(scope="session")
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
---
+++ test/test_add_group.py
@@ -0,0 +1,20 @@
+from model.group import Group
+import pytest
+from fixture.application import Application
+
+
+@pytest.fixture(scope="session")
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+def test_add_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.create(Group(name="First", header="First_header", footer="First_footer"))
+    app.session.logout()
+
+def test_empty_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.create(Group(name="", header="", footer=""))
+    app.session.logout()
